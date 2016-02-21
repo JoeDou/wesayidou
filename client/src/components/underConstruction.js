0dc5e1da8abeacf7233c4d202ba51020c8710d36
Joe Dou <joe.dou@gmail.com>
2016-02-20 18:08:59 -0800

    update message and webpack

--- client/src/components/underConstruction.js
+++ client/src/components/underConstruction.js
@@ -3,7 +3,8 @@ import React from 'react'
 export default () => (
   <div className="wrapper">
     <div className="title">
-      <h2>Under Construction...</h2>
+      <h2>Building a Happily Ever After...</h2>
+      <h2>Please Check Back</h2>
     </div>
     <img src={'/assets/images/underConstruction.jpg'} className="img-responsive">
     </img>
---
+++ webpack.dev.config.js
@@ -0,0 +1,21 @@
+var path = require('path')
+var ROOT_PATH = path.resolve(__dirname +'/client')
+
+module.exports = {
+  entry: [
+    './client/src/index.js'
+  ],
+  output: {
+    filename: path.resolve(ROOT_PATH, 'bundle.js')
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  watch: true
+}
