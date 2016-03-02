fa8274563bc36c5a753807ae40e87184bba40b98
Joe Dou <joe.dou@gmail.com>
2016-03-02 00:09:57 -0800

    fix app.js

--- client/src/reducers/reducer_gallery.js
+++ client/src/reducers/reducer_gallery.js
@@ -3,7 +3,6 @@ import { SHOW_GALLERY } from '../actions/index'
 const INITIAL_STATE = { gallery: { show: false } }
 
 export default function(state=INITIAL_STATE, action){
-  console.log('in reducer')
   switch (action.type) {
     case SHOW_GALLERY:
       return { 
